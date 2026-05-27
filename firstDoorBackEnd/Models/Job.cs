bb2dd57fe0af180e14fb946209bacf51e803a30a
Ethan Berry <ethanberry670@gmail.com>
2026-05-27 09:45:07 +0100

    Add job model

---
+++ firstDoorBackEnd/Models/Job.cs
@@ -0,0 +1,11 @@
+﻿namespace firstDoorBackEnd.Models
+{
+    public class Job
+    {
+        public string Title { get; set; } = string.Empty;
+        public string EmployerName { get; set; } = string.Empty;
+        public string Location {  get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Url {  get; set; } = string.Empty;
+    }
+}
