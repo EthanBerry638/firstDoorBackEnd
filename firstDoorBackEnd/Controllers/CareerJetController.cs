4166a395a1e64dff63f91c4df5516ddd9463001e
viccyp <vic-vlp@outlook.com>
2026-05-27 13:08:28 +0100

    CareerJetController has GetAllJobsAsync() which calls that from the Service, with passing test

--- firstDoorBackEnd/Controllers/CareerJetController.cs
+++ firstDoorBackEnd/Controllers/CareerJetController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using firstDoorBackEnd.Services;
+using firstDoorBackEnd.Models;
 
-namespace firstDoorBackEnd
+namespace firstDoorBackEnd.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CareerJetController
+    public class CareerJetController : ControllerBase
     {
+        private readonly ICareerJetService _careerJetService;
 
+        public CareerJetController(ICareerJetService careerJetService)
+        {
+            _careerJetService = careerJetService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllJobsAsync()
+        {
+           var jobs = await _careerJetService.GetAllJobsAsync();
+           return Ok(jobs);
+        }
     }
 }
