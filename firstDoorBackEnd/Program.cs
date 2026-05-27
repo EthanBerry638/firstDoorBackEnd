37d05a00c1b9336aadce80a147722f75e4432dc6
Ethan Berry <ethanberry670@gmail.com>
2026-05-27 11:21:20 +0100

    WIP: Register http client

--- firstDoorBackEnd/Program.cs
+++ firstDoorBackEnd/Program.cs
@@ -1,4 +1,13 @@
+using firstDoorBackEnd.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddHttpClient<ICareerJetRepository, CareerJetRepository>(client =>
+{
+    client.BaseAddress = new Uri("https://placeholder.com");
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
