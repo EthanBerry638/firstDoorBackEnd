c23e6f3240bc7a6e8f38e13844dd05d86322f94f
Ethan Berry <ethanberry670@gmail.com>
2026-05-27 11:23:42 +0100

    Add http client to repository instructions

--- firstDoorBackEnd/Repositories/CareerJetRepository.cs
+++ firstDoorBackEnd/Repositories/CareerJetRepository.cs
@@ -4,6 +4,13 @@ namespace firstDoorBackEnd.Repositories
 {
     public class CareerJetRepository : ICareerJetRepository
     {
+        private readonly HttpClient _httpClient;
+
+        public CareerJetRepository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public Task<List<Job>>? GetJobsAsync()
         {
             return null;
