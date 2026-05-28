c436acfffaec5baf45806850869a7be62f1c1f19
viccyp <vic-vlp@outlook.com>
2026-05-28 15:28:46 +0100

    Refactor SetUp to include _httpContext

--- firstDoorBackEnd.Tests/CareerJetControllerTests.cs
+++ firstDoorBackEnd.Tests/CareerJetControllerTests.cs
@@ -10,12 +10,14 @@ public class CareerJetControllerTests
 {
     private CareerJetController _careerJetController;
     private Mock<ICareerJetService> mockCareerJetService;
+    private HttpContext _httpContext;
 
     [SetUp]
     public void Setup()
     {
         mockCareerJetService = new Mock<ICareerJetService>();
         _careerJetController = new CareerJetController(mockCareerJetService.Object);
+        _httpContext = new DefaultHttpContext();
     }
 
     [Test]
@@ -60,6 +62,24 @@ public class CareerJetControllerTests
         _careerJetController.ControllerContext = new ControllerContext()
         {
             HttpContext = httpContext
+        };
+
+        var result = _careerJetController.GetUserIPAndUserAgent();
+
+        Assert.That(result.Item1, Is.EqualTo("127.0.0.1"));
+        Assert.That(result.Item2, Is.EqualTo("Mozilla/5.0"));
+    }
+
+    [Test]
+    public async Task GetUserIPAndUserAgent_ReturnsEmptyStrings_WhenValuesAreMissing()
+    {
+
+        _httpContext.Connection.RemoteIpAddress = System.Net.IPAddress.Parse("127.0.0.1");
+        _httpContext.Request.Headers["User-Agent"] = "Mozilla/5.0";
+
+        _careerJetController.ControllerContext = new ControllerContext()
+        {
+            HttpContext = _httpContext
         };
 
         var result = _careerJetController.GetUserIPAndUserAgent();
