7b3dd25ec865195b8a8183a78c7cd8514d79ed2e
Ethan Berry <ethanberry670@gmail.com>
2026-05-28 10:17:59 +0100

    Feat: Add exception handling for career jet forbidden. Won't work for now though as it doesnt exist

--- firstDoorBackEnd/Middleware/ExceptionHandlingMiddleware.cs
+++ firstDoorBackEnd/Middleware/ExceptionHandlingMiddleware.cs
@@ -4,7 +4,20 @@
     {
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            await next.Invoke(context);
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (CareerJetForbiddenException ex)
+            {
+                HandleCareerJetForbiddenException(context, ex);
+            }
+        }
+
+        private async Task HandleCareerJetForbiddenException(HttpContext context, Exception ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status403Forbidden;
+            await context.Response.WriteAsJsonAsync(new { ex.Message });
         }
     }
 }
