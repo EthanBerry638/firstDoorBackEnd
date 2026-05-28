7a30955d1ab166b37826c59475a53da39f553443
Ethan Berry <ethanberry670@gmail.com>
2026-05-28 10:13:59 +0100

    Feat: Add basic setup for new middleware

---
+++ firstDoorBackEnd/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,10 @@
+﻿namespace firstDoorBackEnd.Middleware
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await next.Invoke(context);
+        }
+    }
+}
--- firstDoorBackEnd/Program.cs
+++ firstDoorBackEnd/Program.cs
@@ -1,9 +1,15 @@
+using firstDoorBackEnd.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddTransient<ExceptionHandlingMiddleware>();
+
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
