589f8ce47a291c296ff35c20a98eca54a735ea1a
Carolina Lupanciuc <103419236+karolc222@users.noreply.github.com>
2026-05-28 11:31:27 +0100

    Creating the repository interface for Reed

--- firstDoorBackEnd/Repositories/IReedRepository.cs
+++ firstDoorBackEnd/Repositories/IReedRepository.cs
@@ -1,8 +1,11 @@
 ﻿using firstDoorBackEnd.Models;
+
 namespace firstDoorBackEnd.Repositories
 {
     public interface IReedRepository
     {
+       Task<List<Job>> GetJobsAsync(string keyword);
+
        
     }
 }
